1c242907ce1f58b117f420552e9ca6afd2faf8e0
Andreas Basurto <1514352+nitrohorse@users.noreply.github.com>
2018-03-11 20:55:05 -0700

    Trim numeric amounts for consistency
    Like on CoinMarketCap.com if price is greater than or equal to $1, trim decimal value to hundredths place. Else trim to millionths.

--- server.js
+++ server.js
@@ -44,13 +44,26 @@ const formatPercentage = percentage => {
 }
 
 const formatAmount = amount => {
+  const parts = amount.split('.')
+
+  if (parts[1]) {
+    if (parts[0] > 0) { // if greater than or equal to $1
+      // trim to hundreths place
+      parts[1] = parts[1].slice(0, 2)
+    } else {
+      // trim to millionths place
+      parts[1] = parts[1].slice(0, 6)
+    }
+  }
+
   if (amount < 1000) {
+    amount = `${parts[0]}.${parts[1]}`
     return amount
   }
 
   // add commas
   // https://stackoverflow.com/a/10899795
-  const parts = amount.split('.')
+  // const parts = amount.split('.')
   let amountWithCommas = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',') + (parts[1] ? '.' + parts[1] : '')
 
   // if amount ends with .0 (such as a typical market cap amount)
